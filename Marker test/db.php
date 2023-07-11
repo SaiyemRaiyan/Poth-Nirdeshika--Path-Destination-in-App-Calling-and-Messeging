44ee45c6b89e16a395b10bc0a5baa73fdd05356d
Saiyem Raiyan <64364859+SaiyemRaiyan@users.noreply.github.com>
2023-07-11 14:53:50 +0600

    Add files via upload

--- Marker test/db.php
+++ Marker test/db.php
@@ -1,1 +1,15 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="expertlocation";
 
+$conn=new mysqli($servername,$username,$password,$dbname);
+
+if($conn->connect_error){
+	die("Connection Failed".$conn->connect_error);
+}else{
+	//echo "connected";
+}
+
+?>
---
+++ Marker test/xml.php
@@ -0,0 +1,45 @@
+<?php
+require("db.php");
+
+function parseToXML($htmlStr)
+{
+$xmlStr=str_replace('<','&lt;',$htmlStr);
+$xmlStr=str_replace('>','&gt;',$xmlStr);
+$xmlStr=str_replace('"','&quot;',$xmlStr);
+$xmlStr=str_replace("'",'&#39;',$xmlStr);
+$xmlStr=str_replace("&",'&amp;',$xmlStr);
+return $xmlStr;
+}
+
+
+// Select all the rows in the markers table
+$query = "SELECT * FROM markers";
+$result = mysqli_query($conn,$query);
+if (!$result) {
+  die('Invalid query: ' . mysqli_error());
+}
+
+header("Content-type: text/xml");
+
+// Start XML file, echo parent node
+echo "<?xml version='1.0' ?>";
+echo '<markers>';
+$ind=0;
+// Iterate through the rows, printing XML nodes for each
+while ($row = @mysqli_fetch_assoc($result)){
+  // Add to XML document node
+  echo '<marker ';
+  echo 'id="' . $row['id'] . '" ';
+  echo 'name="' . parseToXML($row['name']) . '" ';
+  echo 'address="' . parseToXML($row['phone']) . '" ';
+  echo 'lat="' . $row['lat'] . '" ';
+  echo 'lng="' . $row['lng'] . '" ';
+  echo 'type="' . $row['type'] . '" ';
+  echo '/>';
+  $ind = $ind + 1;
+}
+
+// End XML file
+echo '</markers>';
+
+?>
